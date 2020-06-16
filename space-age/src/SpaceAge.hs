6325269f4f38e99fcc2d228330d0ac8c1da8df60
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-06-16 21:19:24 +0300

    Fix spelling

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -13,15 +13,15 @@ data Planet
   | Uranus
   | Neptune
 
-calulate :: Float -> Float -> Float
-calulate years secconds= (secconds / 31557600) / years
+calculate :: Float -> Float -> Float
+calculate years secconds= (secconds / 31557600) / years
 
 ageOn :: Planet -> Float -> Float
-ageOn Mercury = calulate 0.2408467
-ageOn Venus   = calulate  0.61519726
-ageOn Earth   = calulate   1
-ageOn Mars    = calulate   1.8808158
-ageOn Jupiter = calulate  11.862615
-ageOn Saturn  = calulate  29.447498
-ageOn Uranus  = calulate  84.016846
-ageOn Neptune = calulate  164.79132
+ageOn Mercury = calculate 0.2408467
+ageOn Venus   = calculate  0.61519726
+ageOn Earth   = calculate   1
+ageOn Mars    = calculate   1.8808158
+ageOn Jupiter = calculate  11.862615
+ageOn Saturn  = calculate  29.447498
+ageOn Uranus  = calculate  84.016846
+ageOn Neptune = calculate  164.79132
