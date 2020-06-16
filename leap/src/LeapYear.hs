70539de953dfe69aaf555a8fbb7003c6675e0641
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-06-16 19:27:29 +0300

    First commit

---
+++ leap/src/LeapYear.hs
@@ -0,0 +1,10 @@
+module LeapYear
+  ( isLeapYear
+  ) where
+
+isLeapYear :: Integer -> Bool
+isLeapYear year
+  | (mod year 4 == 0) && (mod year 100 == 0) && (mod year 400 == 0) = True
+  | (mod year 4 == 0) && (mod year 100 == 0) = False
+  | (mod year 4 == 0) = True
+  | otherwise = False
