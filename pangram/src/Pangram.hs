67243ad0b89817972e5aa03822cc52fbf158c3df
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-06-17 00:27:13 +0300

    Reformat

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -1,12 +1,11 @@
 module Pangram (isPangram) where
 
-import           Data.Char (isDigit, toLower)
-
+import Data.Char (isDigit, toLower)
 
 removeLetters :: Char -> String -> String
 removeLetters l = filter (/= l)
 
-hasLetter:: Char -> String -> Bool
+hasLetter :: Char -> String -> Bool
 hasLetter = elem
 
 toLoverCase :: String -> String
@@ -14,28 +13,29 @@ toLoverCase = fmap toLower
 
 removeNumbers :: [String] -> [String]
 removeNumbers = filter isNotNumber
-  where isNotNumber str = not $ all isDigit  str
+  where
+    isNotNumber str = not $ all isDigit str
 
 removeDashes :: String -> String
-removeDashes = filter (/='_')
+removeDashes = filter (/= '_')
 
 removeDots :: String -> String
-removeDots = filter (/='.')
+removeDots = filter (/= '.')
 
 removeQuotes :: String -> String
-removeQuotes = filter (/='"')
+removeQuotes = filter (/= '"')
 
 removePunctuation :: String -> String
 removePunctuation = removeDashes . removeDots . removeQuotes
 
 isPangram :: String -> Bool
-isPangram text = process ['a'..'z'] $  (toLoverCase . removePunctuation . concat . removeNumbers . words) text
-    where process letters txt =
-            case (letters, txt) of
-            (_:_, []) -> False
-            ([], _) -> True
-            (x:xs, t) ->
-              if hasLetter x t
-              then process xs (removeLetters x t)
-              else
-                False
+isPangram text = process ['a' .. 'z'] $ (toLoverCase . removePunctuation . concat . removeNumbers . words) text
+  where
+    process letters txt =
+      case (letters, txt) of
+        (_ : _, []) -> False
+        ([], _) -> True
+        (x : xs, t) ->
+          if hasLetter x t
+            then process xs (removeLetters x t)
+            else False
