f6382ffc88ff270fe6e681fd5fe1f13e62152cc2
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-06-17 21:14:24 +0300

    Add solution without if

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,7 +1,7 @@
 module CollatzConjecture (collatz) where
 
-collatz :: Integer -> Maybe Integer
-collatz num
+collatz' :: Integer -> Maybe Integer
+collatz' num
   | num <= 0 = Nothing
   | otherwise = go num 0
     where
@@ -12,3 +12,13 @@ collatz num
             if n `mod` 2 == 0
               then go (n `div` 2) (count + 1)
               else go (n * 3 + 1) (count + 1)
+
+collatz :: Integer -> Maybe Integer
+collatz num 
+  | num < 1 = Nothing
+  | otherwise = go num 0
+    where
+      go n count 
+        | n == 1 = Just count
+        | even n = go (n `div` 2) (count +1)
+        | otherwise = go (n *3 +1) (count +1)
