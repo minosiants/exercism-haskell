8aad8c1b915a8632612da10fc4ad70010146ceb5
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-06-23 20:57:09 +0300

    Phone

--- phone-number/src/Phone.hs
+++ phone-number/src/Phone.hs
@@ -1,4 +1,14 @@
 module Phone (number) where
 
+import Data.Char (isNumber)
+
 number :: String -> Maybe String
-number xs = error "You need to implement this function."
+number = validate . filter isNumber
+  where
+    validate :: String -> Maybe String
+    validate num
+      | length num < 10 = Nothing
+      | reverse num !! 6 < '2' || reverse num !! 9 < '2' = Nothing
+      | head num == '1' && (length . tail) num == 10 = Just $ tail num
+      | length num == 10 = Just num
+      | otherwise = Nothing
