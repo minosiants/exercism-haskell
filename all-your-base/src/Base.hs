ed1264398f9ea87c79f87e41ee16f11de5848c4c
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-07-11 02:20:11 +0300

    All-base

--- all-your-base/src/Base.hs
+++ all-your-base/src/Base.hs
@@ -1,15 +1,15 @@
 module Base (Error (..), rebase) where
 
 import Data.List (find)
-import Debug.Trace
 
 data Error a = InvalidInputBase | InvalidOutputBase | InvalidDigit a
   deriving (Show, Eq)
 
+zipWithIndex :: [a] -> [(a, Int)]
+zipWithIndex xs = zip xs (reverse [0 .. (length xs) -1])
+
 sum' :: Integral a => [a] -> a
-sum' n =
-  let zipped = zip n (reverse [0 .. (length n) -1])
-   in foldr (\(el, i) acc -> el * 10 ^ i + acc) 0 zipped
+sum' xs = foldr (\(el, i) acc -> el * 10 ^ i + acc) 0 (zipWithIndex xs)
 
 validateDigits :: Integral a => a -> [a] -> Either (Error a) [a]
 validateDigits base digits =
@@ -21,15 +21,16 @@ rebase :: Integral a => a -> a -> [a] -> Either (Error a) [a]
 rebase inputBase outputBase inputDigits
   | inputBase < 2 = Left InvalidInputBase
   | outputBase < 2 = Left InvalidOutputBase
-  | inputBase == 10 = (convert outputBase) . sum' <$> (validateDigits inputBase inputDigits)
-  | otherwise = (convert outputBase) . (to10Base inputBase) <$> (validateDigits inputBase inputDigits)
+  | inputBase == 10 = convertOutBase . sum' <$> validated
+  | otherwise = convertOutBase . (to10Base inputBase) <$> validated
   where
+    validated = validateDigits inputBase inputDigits
+    convertOutBase = convert outputBase
     convert _ 0 = []
     convert base num =
-          let res = num `div` base
-              rest = num `mod` base
-           in (convert base res) ++ [rest]
+      let res = num `div` base
+          rest = num `mod` base
+       in (convert base res) ++ [rest]
     to10Base base digits =
-      let zipped = zip digits $ reverse [0 .. (length digits) - 1]
-       in sum $
-            foldr (\(d, i) acc -> (d * base ^ i) : acc) [] zipped
+      sum $
+        foldr (\(d, i) acc -> (d * base ^ i) : acc) [] (zipWithIndex digits)
