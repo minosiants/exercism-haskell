f571a98115d05562e08a696068c5dad57d2c61e0
Kaspar Minosiants <kaspar.ru@gmail.com>
2021-02-07 23:33:38 +0300

    Simplify Proverb

--- proverb/src/Proverb.hs
+++ proverb/src/Proverb.hs
@@ -5,7 +5,5 @@ recite [] = ""
 recite xs = mkLine $ init xs `zip` tail xs
   where
     mkLine [] = "And all for the want of a " ++ (head xs) ++ "."
-    mkLine words' =
-      let (w1, w2) = head words'
-       in "For want of a " ++ w1 ++ " the " ++ w2 ++ " was lost.\n"
-            ++ mkLine (tail words')
+    mkLine ((w1, w2) : xs') =
+      "For want of a " ++ w1 ++ " the " ++ w2 ++ " was lost.\n" ++ mkLine xs'
