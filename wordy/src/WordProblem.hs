ea492ad4fc2430e775c7ef36b6eedc71e340ee97
Kaspar Minosiants <kaspar.ru@gmail.com>
2021-04-01 23:01:46 +0300

    wordy

--- wordy/src/WordProblem.hs
+++ wordy/src/WordProblem.hs
@@ -1,23 +1,55 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module WordProblem (answer) where
-import Debug.Trace
-import Control.Applicative
-import Data.Char
+
+import Data.Char (isNumber)
+import Data.Maybe (isJust, maybeToList)
+import Debug.Trace (trace)
 
 data Operation
   = Plus
   | Minus
   | Multiply
   | Divide
+  | Unsupported
   deriving (Show, Eq)
 
+data WordType
+  = Ops
+  | Num
+  deriving (Eq, Show)
 
-parse :: String -> Maybe Integer
-parse str =
+isTwoWordsOp :: Operation -> Bool
+isTwoWordsOp op =
+  case op of
+    Divide -> True
+    Multiply -> True
+    _ -> False
+
+parseAll :: String -> Maybe Integer
+parseAll str =
   case words str of
     ("What" : "is" : xs) ->
-      number' (head (trace("xs: "++ show xs)(xs))) <|> expression xs
+      parse xs
     _ -> Nothing
+
+parse :: [String] -> Maybe Integer
+parse xs
+  | Unsupported `elem` ops = Nothing
+  | null ops && null nums = Nothing
+  | null ops = Just $ head nums
+  | otherwise = Just $ go nums ops
+  where
+    nums = parseNumbers xs
+    ops = parseOperations xs
+    go :: [Integer] -> [Operation] -> Integer
+    go xs op =
+      fst $
+        foldl
+          ( \(acc, opp) x ->
+              let res = operation (head opp) acc x
+               in (res, tail opp)
+          )
+          (head xs, op)
+          (tail xs)
 
 operation :: Operation -> Integer -> Integer -> Integer
 operation op x y =
@@ -26,46 +58,39 @@ operation op x y =
     Minus -> x - y
     Multiply -> x * y
     Divide -> x `div` y
+    Unsupported -> error ""
 
-expression :: [String] -> Maybe Integer
-expression xs =
-  if length xs < 3
-    then Nothing
-    else do
-      n1 <- parseNumber (head xs)
-      op <- parseOperation (xs !! 1)
-      operation op n1 <$>
-        ( if op == Multiply || op == Divide
-            then parseNumber (xs !! 3)
-            else
-              parseNumber
-                ( xs !! 2
-                )
-          )
+parseNumbers :: [String] -> [Integer]
+parseNumbers xs = concat $ maybeToList $sequence $ filter isJust $ fmap parseNumber xs
 
-parseOperation :: String -> Maybe Operation
+parseOperations :: [String] -> [Operation]
+parseOperations xs = go xs Num
+  where
+    go [] _ = []
+    go (x : xs) wt =
+      case wt of
+        Ops ->
+          let op = parseOperation x
+              xs' = if isTwoWordsOp op then tail xs else xs
+           in op : go xs' Num
+        Num -> go xs Ops
+
+parseOperation :: String -> Operation
 parseOperation str
-  | str == "plus" = Just Plus
-  | str == "minus" = Just Minus
-  | str == "divided" = Just Divide
-  | str == "multiplied" = Just Multiply
-  | otherwise = Nothing
-
-number' :: String -> Maybe Integer
-number' xs =
-  let s = if last xs == '?'
-            then parseNumber xs
-            else Nothing
-  in trace ("num: "++ show s ++ "xs: " ++show xs)s   
+  | str == "plus" = Plus
+  | str == "minus" = Minus
+  | str == "divided" = Divide
+  | str == "multiplied" = Multiply
+  | otherwise = Unsupported
 
 parseNumber :: String -> Maybe Integer
-parseNumber n@(x:xs)
-  | x == '-' =fmap  (\x -> x * (-1)) (go xs)
+parseNumber n@(x : xs)
+  | x == '-' = fmap (* (-1)) (go xs)
   | otherwise = go n
-  where 
-    go xs = 
-        let r = takeWhile isNumber xs
-        in if null r then Nothing else Just $ read r
+  where
+    go xs =
+      let r = takeWhile isNumber xs
+       in if null r then Nothing else Just $ read r
 
 answer :: String -> Maybe Integer
-answer = parse
+answer = parseAll
