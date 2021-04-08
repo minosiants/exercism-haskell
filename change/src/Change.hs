ee762375cfa09768b30a57da9932280dd3fdf2af
Kaspar Minosiants <kaspar.ru@gmail.com>
2021-04-08 18:21:23 +0300

    Change

--- change/src/Change.hs
+++ change/src/Change.hs
@@ -1,50 +1,18 @@
 module Change (findFewestCoins) where
 
 import Data.List
-import Debug.Trace
-import Control.Applicative
+import Data.Function
+import Data.Maybe
 import Control.Monad
 
-import Data.Maybe
-
-
-
-findCoins :: Integer ->[Integer] -> Maybe [Integer]
-findCoins _ [] = Nothing
-findCoins 0 _ = Just []
-findCoins target coins  = do
-            coin <- findCoin target coins'
-            let diff  = target - coin
-            let diff' = trace ("diff " ++ show diff ++  " coin "++ show coin ++ " coins" ++ show coin ++ " coins' "++ show coins'++ " target " ++ show target) diff
-            c <- if diff == 0 then pure [] else  findCoins diff' coins'
-            return $ coin : c
-    where
-        coins' = filter (<=target) coins
-
-findCoin :: Integer -> [Integer] -> Maybe Integer
-findCoin _ [] = Nothing
-findCoin target coins =
-           let coins' = filter (<= target) coins
-               candidate  = last coins'
-               diff = target - candidate
-               check =  filter (<= diff) coins
-               msg = trace("coins' "++ show coins' ++ " candidate "  ++ show candidate ++ " diff "++ show diff++" check "++ show check)(check)
-            in
-                if null msg  && diff /= 0
-                then findCoin target (init coins')
-                else Just candidate
-
-
 findFewestCoins :: Integer -> [Integer] -> Maybe [Integer]
-findFewestCoins _ [] =Nothing
-findFewestCoins target coins =
-    let
-        rest = findFewestCoins target (init coins)
-        allCases = sequence $ filter isJust $ findCoins target coins : [rest]
-        v = do
-          ac <- allCases
-          let r = fmap snd $ sortOn fst $fmap (\x -> (length x, x)) ac 
-          if null r then Nothing else return $ head r  
+findFewestCoins target coins
+  | target < 0  = Nothing
+  | target == 0 = Just []
+  | otherwise   = selectShortest $ catMaybes $ f <$> filter (<=target) coins
+  where f c = let ns = target`div`c in msum $ g c <$> [ns,ns-1..1]
+        g c n = (replicate (fromIntegral n) c ++) <$> findFewestCoins (target-c*n) coins
 
-    in v
-
+selectShortest :: [[a]] -> Maybe [a]
+selectShortest [] = Nothing
+selectShortest xs = Just $ minimumBy (compare`on`length) xs
